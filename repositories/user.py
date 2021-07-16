0db2bc194a15660554715d996325311b33b6466e
pheni-18 <pheni.engineer@gmail.com>
2021-07-16 18:27:54 +0900

    Use abc

--- repositories/user.py
+++ repositories/user.py
@@ -1,3 +1,7 @@
-class IUserRepository:
+from abc import ABCMeta, abstractmethod
+
+
+class IUserRepository(metaclass=ABCMeta):
+    @abstractmethod
     def get_user_by_name(self, user_name: str):
         pass
